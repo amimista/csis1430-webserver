cc7139183ed2a01a879c4307b264a531da2c247b
amimista <77865981+amimista@users.noreply.github.com>
2022-11-12 18:10:12 -0700

    update lottery to be B E T T E R

--- pages/lottery/js/index.js
+++ pages/lottery/js/index.js
@@ -1,16 +1,37 @@
-let lotteryLength = prompt("How many numbers do you want to pick?");
+let lotteryLength;
+let lotteryMin;
+let lotteryMax;
+const absoluteMax = 20;
 
-while(parseInt(lotteryLength) >= 6) {
-  lotteryLength = prompt("That number is too high! How many numbers do you want to pick?");
+function lottery() {
+  document.getElementById("result").style.opacity = 0;
+  document.getElementById("result").style.transform = "translateY(-30%)";
+
+  lotteryLength = parseInt(document.getElementById("inputLength").value);
+  if(lotteryLength == null || lotteryLength == NaN) {
+    lotteryLength = 6;
+  }
+  lotteryMin = Math.ceil(parseFloat(document.getElementById("inputRange1").value));
+  if(lotteryMin == null || lotteryMin == NaN) {
+    lotteryMin = 1;
+  }
+  lotteryMax = Math.floor(parseFloat(document.getElementById("inputRange2").value));
+  if(lotteryMax == null || lotteryMax == NaN) {
+    lotteryMax = 1;
+  }
+
+  if(lotteryLength > absoluteMax) {
+    lotteryLength = 20;
+  }
+  let lotteryArray = new Array(lotteryLength);
+  let resultOutputList = new Array();
+  
+  for (let index = 0; index < lotteryArray.length; index++) {
+    lotteryArray[index] = Math.floor(Math.random() * (lotteryMax - lotteryMin + 1) + lotteryMin);
+    resultOutputList.push(`${lotteryArray[index]}`);
+  }
+
+  document.getElementById("result").innerText = resultOutputList.join("-");
+  document.getElementById("result").style.opacity = 1;
+  document.getElementById("result").style.transform = "translateY(0)";
 }
-
-let lottery = new Array(parseInt(lotteryLength));
-let resultOutputList = new Array();
-
-for (let index = 0; index < lottery.length; index++) {
-  lottery[index] = Math.ceil(Math.random() * 99);
-  resultOutputList.push(`${lottery[index]}`);
-}
-document.getElementById("result").style.opacity = 1;
-document.getElementById("result").style.transform = "translateY(0)";
-document.getElementById("result").innerText = resultOutputList.join("-");
